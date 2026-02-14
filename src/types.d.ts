b012f29763c3c44debb62de36c01bee44849a0e5
Rahul Dey <rahuldey9623@gmail.com>
2026-02-14 18:19:08 +0000

    Added auth middleware

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,13 @@
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { todos } from "@/db/schema";
+import { auth } from "@/lib/auth";
 
 export type TodoDTO = InferSelectModel<typeof todos>;
 export type AddTodoDTO = InferInsertModel<typeof todos>;
+
+export type HonoEnv = {
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+};
